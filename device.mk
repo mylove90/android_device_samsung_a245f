3c3992ab3a724d63ceeb82d3e5b07e073f53e38c
diamdosnf yuuguu <diamondguys256x@gmail.com>
2024-08-03 11:44:02 +0000

    a245f: Build Health Hal 2.1

--- device.mk
+++ device.mk
@@ -18,5 +18,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
